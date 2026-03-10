a126742c1d1b15e1e76d3eff8a666efa3801fac0
Salman Nazeer <salmannazeer477@gmail.com>
2026-03-10 21:17:51 +0530

    Reworked the code to download CS papers

--- index.js
+++ index.js
@@ -25,7 +25,28 @@ async function downloadZip(url, outputFolder = "./downloads", filename) {
 }
 
 
-for(let i = 2022; i < 2026; i++){
-    downloadZip(`https://www.cbse.gov.in/cbsenew/question-paper/${i}-COMPTT/XII/Physics.zip`, "./downloads", `physics_${i}_supplementary.zip`)
-  .catch(console.error);
+
+const subject = "cs"
+// main paper https://www.cbse.gov.in/cbsenew/question-paper/2022/XII/Computer_Science.zip
+// supplementary paper https://www.cbse.gov.in/cbsenew/question-paper/2022-COMPTT/XII/computer_science.zip
+// main scheme https://www.cbse.gov.in/cbsenew/Marking-Scheme/2022/XII/COMPUTER_SCIENCE.zip
+// supplementary scheme https://www.cbse.gov.in/cbsenew/marking-scheme/2022-COMPTT/XII/Computer_Science.zip
+for (let i = 2022; i < 2026; i++) {
+  downloadZip(`https://www.cbse.gov.in/cbsenew/question-paper/${i}/XII/Computer_Science.zip`, `./downloads/`, `${subject}_${i}.zip`)
+    .catch(console.error);
+  downloadZip(`https://www.cbse.gov.in/cbsenew/question-paper/${i}-COMPTT/XII/computer_science.zip`, `./downloads/`, `${subject}_${i}_supplementary.zip`)
+    .catch(console.error);
+  downloadZip(`https://www.cbse.gov.in/cbsenew/Marking-Scheme/${i}/XII/COMPUTER_SCIENCE.zip`, `./downloads/`, `${subject}_${i}_scheme.zip`)
+    .catch(console.error);
+  downloadZip(`https://www.cbse.gov.in/cbsenew/marking-scheme/${i}-COMPTT/XII/Computer_Science.zip`, `./downloads/`, `${subject}_${i}_supplementary_scheme.zip`)
+    .catch(console.error);
+  // for 91_computer_science.zip
+  downloadZip(`https://www.cbse.gov.in/cbsenew/question-paper/${i}/XII/91_Computer_Science.zip`, `./downloads/`, `${subject}_${i}.zip`)
+    .catch(console.error);
+  downloadZip(`https://www.cbse.gov.in/cbsenew/question-paper/${i}-COMPTT/XII/91_computer_science.zip`, `./downloads/`, `${subject}_${i}_supplementary.zip`)
+    .catch(console.error);
+  downloadZip(`https://www.cbse.gov.in/cbsenew/Marking-Scheme/${i}/XII/91_COMPUTER_SCIENCE.zip`, `./downloads/`, `${subject}_${i}_scheme.zip`)
+    .catch(console.error);
+  downloadZip(`https://www.cbse.gov.in/cbsenew/marking-scheme/${i}-COMPTT/XII/91_Computer_Science.zip`, `./downloads/`, `${subject}_${i}_supplementary_scheme.zip`)
+    .catch(console.error);
 }
